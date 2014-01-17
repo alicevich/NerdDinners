f74e45605d33ec09a42978f95eeb889cc7af93ef
Alice Vichitthavong <alice.vich@gmail.com>
2014-01-17 13:14:27 -0800

    first commit

---
+++ NerdDinner/Models/ControllerHelpers.cs
@@ -0,0 +1,14 @@
+﻿using NerdDinner.Models;
+using System.Collections.Generic;
+using System.Web.Http.ModelBinding;
+public static class ControllerHelpers
+{
+    public static void AddRuleViolations(this ModelStateDictionary modelState,
+    IEnumerable<RuleViolation> errors)
+    {
+        foreach (RuleViolation issue in errors)
+        {
+            modelState.AddModelError(issue.PropertyName, issue.ErrorMessage);
+        }
+    }
+}
