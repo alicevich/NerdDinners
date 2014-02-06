cedaa4cf434975feb09300bf4e03926e28c52192
Alice Vichitthavong <alice.vich@gmail.com>
2014-02-06 10:19:44 -0800

    no message

---
+++ NerdDinner.Tests/Controllers/DinnersControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NerdDinner.Controllers;
+using NerdDinner.Models;
+using Assert = NUnit.Framework.Assert;
+
+namespace NerdDinner.Tests.Controllers
+{
+    [TestFixture]
+    public class DinnersControllerTest
+    {
+       [Test]
+        public void DetailsAction_Should_Return_View_For_ExistingDinner()
+        {
+            // Arrange
+            var controller = new DinnersController();
+            // Act
+            var result = controller.Details(1) as ViewResult;
+            // Assert
+            Assert.IsNotNull(result, "Expected View");
+        }
+
+        [TestMethod]
+            public void DetailsAction_Should_Return_NotFoundView_For_BogusDinner() {
+            // Arrange
+            var controller = new DinnersController();
+            // Act
+            var result = controller.Details(999) as ViewResult;
+            // Assert
+            Assert.AreEqual("NotFound", result.ViewName);
+        }
+    }
+}
---
+++ NerdDinner.Tests/Models/DinnerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NerdDinner.Models;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace NerdDinner.Tests.Models
+{
+    [TestFixture]
+    public class DinnerTest
+    {
+        [Test]
+        public void Dinner_Should_Not_Be_Valid_When_Some_Properties_Incorrect()
+        {
+            //Arrange
+            var dinner = new Dinner
+                {
+                Title = "Test title",
+                Country = "USA",
+                ContactPhone = "BOGUS"
+            };
+            // Act
+            var isValid = dinner.IsValid;
+            //Assert
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void Dinner_Should_Be_Valid_When_All_Properties_Correct()
+        {
+            //Arrange
+            var dinner = new Dinner
+            {
+                Title = "Test title",
+                Description = "Some description",
+                EventDate = DateTime.Now,
+                HostedBy = "ScottGu",
+                Address = "One Microsoft Way",
+                Country = "USA",
+                ContactPhone = "425-703-8072",
+                Latitude = 93,
+                Longitude = -92,
+            };
+            // Act
+            var isValid = dinner.IsValid;
+            //Assert
+            Assert.IsTrue(isValid);
+        }
+    }
+}
